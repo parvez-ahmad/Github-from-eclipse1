6a709115ab4f517c7bd70609ac586d0cf7020802
parvez-ahmad <79909780+parvez-ahmad@users.noreply.github.com>
2021-05-31 15:45:50 +0530

    john changes

--- Github-from-eclipse1/src/main/java/com/javatechie/Github_from_eclipse1/App.java
+++ Github-from-eclipse1/src/main/java/com/javatechie/Github_from_eclipse1/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+         System.out.println( "changes done by john" );
         System.out.println( "Hello World!"+" first change from java techie" );
     }
 }
